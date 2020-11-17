9ce9cccf38391a599706cd7e94dda3174c820d62
Sarah Wood <wood.sarah@outlook.com>
2020-11-17 19:55:37 +0000

    LazyHero updated

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import LazyHero from 'react-lazy-hero';
 
+export default MyLandingPage() {
+  return (
+      <div>
+          <LazyHero imageSrc="https://unsplash.it/2000/1000">
+              <h1>Generic Startup Hype Headline</h1>
+          </LazyHero>
+
+          {/* ... */}
+      </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
