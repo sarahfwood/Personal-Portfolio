a70f497a3d3516b29fc887106bebe540bb2bece1
Sarah Wood <wood.sarah@outlook.com>
2020-11-18 20:48:35 +0000

    LazyHero updated

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <section id="intro">
-      <h1>Sarah Wood</h1>
-      <h2>Personal Portfolio</h2>
-      <navbar>
-        <ul>
-          <li>
-          <a href="#about">About Me</a>
-          </li>
-          <li>
-          <a href="#projects">Projects</a>
-          </li>
-          <li>
-          <a href="#skills">Skills</a>
-          </li>
-          <li>
-          <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </navbar>
-      </section>
-    </div>
+    <LandingPage />
+    // <div className="App">
+    //   <section id="intro">
+    //   <h1>Sarah Wood</h1>
+    //   <h2>Personal Portfolio</h2>
+    //   <navbar>
+    //     <ul>
+    //       <li>
+    //       <a href="#about">About Me</a>
+    //       </li>
+    //       <li>
+    //       <a href="#projects">Projects</a>
+    //       </li>
+    //       <li>
+    //       <a href="#skills">Skills</a>
+    //       </li>
+    //       <li>
+    //       <a href="#contact">Contact</a>
+    //       </li>
+    //     </ul>
+    //   </navbar>
+    //   </section>
+    // </div>
     
   );
 }
