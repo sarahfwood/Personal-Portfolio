104a8bdb51d2ec7d57d7c8e484d38e4f7767aea5
Sarah Wood <wood.sarah@outlook.com>
2020-12-16 20:48:58 +0000

    Test function added

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -1,3 +1,10 @@
 const arrayOne = [1, 2, 3, 4];
 const arrayTwo = [4, 5, 6, 7];
 
+for (let i = 0; i < arrayOne.length; i++) {
+  for (let j = 0; j < arrayTwo.length; j++) {
+    if (arrayOne[i] === arrayTwo[j]) {
+      console.log('Both loops have the number: ' + arrayTwo[j]);
+    }
+  }
+}
